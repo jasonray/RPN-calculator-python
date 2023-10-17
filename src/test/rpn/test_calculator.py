76ad14244ea25165d648ec1d82bbd0549182223a
Jason Ray <jason.ray@cerner.com>
2023-10-17 19:22:56 -0400

    formatting

--- src/test/rpn/test_calculator.py
+++ src/test/rpn/test_calculator.py
@@ -1,25 +1,26 @@
 import unittest
 from src.rpn.calculator import RpnCalculator
 
+
 class TestCalculator(unittest.TestCase):
 
     def test_peek(self):
-        calc = RpnCalculator() 
-        self.assertEqual( calc.peek(), 0 )
+        calc = RpnCalculator()
+        self.assertEqual(calc.peek(), 0)
 
     def test_multiple_enter(self):
-        calc = RpnCalculator() 
-        calc.enter( 1 ) 
-        calc.enter( 2 ) 
-        self.assertEqual( calc.peek(), 2 )
+        calc = RpnCalculator()
+        calc.enter(1)
+        calc.enter(2)
+        self.assertEqual(calc.peek(), 2)
 
     def test_enter_then_peek(self):
-        calc = RpnCalculator() 
-        calc.enter( 1 ) 
-        self.assertEqual( calc.peek(), 1 )
+        calc = RpnCalculator()
+        calc.enter(1)
+        self.assertEqual(calc.peek(), 1)
 
     def test_clear(self):
-        calc = RpnCalculator() 
-        calc.enter( 1 ) 
+        calc = RpnCalculator()
+        calc.enter(1)
         calc.clear()
-        self.assertEqual( calc.peek(), 0 )
+        self.assertEqual(calc.peek(), 0)
