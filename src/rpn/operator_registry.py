a5c46c7a9c4d02e794a07a61869c82d31b6164f2
Jason Ray <jason.ray@cerner.com>
2023-10-17 20:43:58 -0400

    operator interface

--- src/rpn/operator_registry.py
+++ src/rpn/operator_registry.py
@@ -1,3 +1,4 @@
+from src.rpn.operator.operator import Operator
 from src.rpn.operator.addition_operator import AdditionOperator
 from src.rpn.operator.subtraction_operator import SubtractionOperator
 
@@ -8,7 +9,7 @@ class OperatorRegistry:
         self._registry.append( AdditionOperator() )
         self._registry.append( SubtractionOperator() )
 
-    def getOperator(self, operatorCharacter):
+    def getOperator(self, operatorCharacter: str) -> Operator:
         relevantOperator = None
         for operator in self._registry:
             if operator.handlesOperatorCharacter(operatorCharacter):
