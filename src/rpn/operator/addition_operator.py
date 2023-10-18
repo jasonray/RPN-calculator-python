9b729fbb581b333805333d23bf908d4b76620db8
Jason Ray <jason.ray@cerner.com>
2023-10-17 21:38:49 -0400

    add binary operator

--- src/rpn/operator/addition_operator.py
+++ src/rpn/operator/addition_operator.py
@@ -1,18 +1,14 @@
 from src.rpn.rpnstack import RpnStack
-from src.rpn.operator.operator import Operator
+from src.rpn.operator.binary_operator import BinaryOperator
 
 
-class AdditionOperator(Operator):
+class AdditionOperator(BinaryOperator):
 
     def __init__(self):
         pass
 
-    def doOperation(self, numbers: RpnStack) -> int:
-        lhs = numbers.pop()
-        rhs = numbers.pop()
-        result = lhs + rhs
-        numbers.push(result)
-        return result
+    def doOperatorByOperands(self, lhs: int, rhs: int) -> int:
+        return lhs+rhs
 
     def handlesOperatorCharacter(self, operand) -> bool:
         PLUS = "+"
---
+++ src/rpn/operator/binary_operator.py
@@ -0,0 +1,15 @@
+from src.rpn.rpnstack import RpnStack
+from src.rpn.operator.operator import Operator
+
+
+class BinaryOperator(Operator):
+
+    def doOperation(self, numbers: RpnStack) -> int:
+        rhs = numbers.pop()
+        lhs = numbers.pop()
+        result = self.doOperatorByOperands(lhs, rhs)
+        numbers.push(result)
+        return result
+
+    def doOperatorByOperands(self, lhs: int, rhs: int) -> int:
+        pass
