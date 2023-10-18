e050a3bfd9c86764a15ebcc8a0e481a5022512a3
Jason Ray <jason.ray@cerner.com>
2023-10-17 22:37:47 -0400

    division test

--- src/test/rpn/operator/test_division.py
+++ src/test/rpn/operator/test_division.py
@@ -30,3 +30,10 @@ class TestDivision(unittest.TestCase):
         calc.enter(2)
         result = calc.perform("/")
         self.assertEqual(result, 4)
+
+    def test_divide_fractional(self):
+        calc = RpnCalculator()
+        calc.enter(6)
+        calc.enter(4)
+        result = calc.perform("/")
+        self.assertEqual(result, 1.5)
