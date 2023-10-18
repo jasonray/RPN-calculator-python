577e26e46b6983a5e25775509ad6489dd054d279
Jason Ray <jason.ray@cerner.com>
2023-10-18 09:16:25 -0400

    average test

--- src/test/rpn/operator/test_average.py
+++ src/test/rpn/operator/test_average.py
@@ -11,6 +11,13 @@ class TestAverage(unittest.TestCase):
 
     def test_average_one_number(self):
         calc = RpnCalculator()
-        calc.push(1)
+        calc.enter(1)
         result = calc.perform("AVE")
         self.assertEqual(result, 1)
+
+    def test_average_two_numbers(self):
+        calc = RpnCalculator()
+        calc.enter(1)
+        calc.enter(3)
+        result = calc.perform("AVE")
+        self.assertEqual(result, 2)
