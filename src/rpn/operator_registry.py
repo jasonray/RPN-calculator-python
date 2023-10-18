99307b59355661e643c8531ea183aff375858d7c
Jason Ray <jason.ray@cerner.com>
2023-10-17 20:45:29 -0400

    formatting

--- src/rpn/operator_registry.py
+++ src/rpn/operator_registry.py
@@ -2,16 +2,17 @@ from src.rpn.operator.operator import Operator
 from src.rpn.operator.addition_operator import AdditionOperator
 from src.rpn.operator.subtraction_operator import SubtractionOperator
 
+
 class OperatorRegistry:
 
     def __init__(self):
         self._registry = []
-        self._registry.append( AdditionOperator() )
-        self._registry.append( SubtractionOperator() )
+        self._registry.append(AdditionOperator())
+        self._registry.append(SubtractionOperator())
 
     def getOperator(self, operatorCharacter: str) -> Operator:
         relevantOperator = None
         for operator in self._registry:
             if operator.handlesOperatorCharacter(operatorCharacter):
-                relevantOperator=operator
+                relevantOperator = operator
         return relevantOperator
