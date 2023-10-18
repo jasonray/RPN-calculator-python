2ed545024e6b6cb04c823ea1f69801a94a530e05
Jason Ray <jason.ray@cerner.com>
2023-10-17 20:13:06 -0400

    addition tests

--- src/test/rpn/operator/test_addition.py
+++ src/test/rpn/operator/test_addition.py
@@ -10,3 +10,7 @@ class TestAdd(unittest.TestCase):
         calc.enter(2)
         result = calc.perform("+")
         self.assertEqual(result, 3)
+    def test_add_no_numbers(self):
+        calc = RpnCalculator()
+        result = calc.perform("+")
+        self.assertEqual(result, 0)
