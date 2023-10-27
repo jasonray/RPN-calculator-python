4a821affd7e80c55a23edfadd9f4c3d5e842c322
Jason Ray <jayray.net@gmail.com>
2023-10-27 17:32:23 -0400

    drop stack operator

--- src/rpn/operator/sum_operator.py
+++ src/rpn/operator/sum_operator.py
@@ -1,10 +1,17 @@
-from src.rpn.operator.stack_operator import StackOperator
+import functools
+from src.rpn.operator.operator import Operator
+from src.rpn.rpnstack import RpnStack
 
 
-class SumOperator(StackOperator):
+class SumOperator(Operator):
 
-    def reduce_operation(self, x: int, y: int) -> int:
-        return x + y
+    def doOperation(self, numbers: RpnStack) -> int:
+        values = numbers.pop_all()
+        result = 0
+        if values:
+            result = int(functools.reduce(lambda x, y: x + y, values))
+            numbers.push(result)
+        return result
 
     def handlesOperatorCharacter(self, operand) -> bool:
         return operand == "sum"
