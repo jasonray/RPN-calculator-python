715a585fa6bd060d82f9beac2bde7f31bfdf1ca7
Jason Ray <jayray.net@gmail.com>
2023-10-28 15:47:32 -0400

    formatting

--- src/test/rpn/test_calculator_buffer_input.py
+++ src/test/rpn/test_calculator_buffer_input.py
@@ -12,16 +12,16 @@ class TestBufferInput(unittest.TestCase):
         return calc
 
     def test_buffer_input_empty(self):
-        self.run_test("",0)
+        self.run_test("", 0)
 
     def test_buffer_input_zero(self):
-        self.run_test("0",0)
+        self.run_test("0", 0)
 
     def test_buffer_input_add(self):
-        self.run_test("1 2 +",3)
+        self.run_test("1 2 +", 3)
 
     def test_buffer_add_sub(self):
-        self.run_test("3 4 - 5 +",4)
+        self.run_test("3 4 - 5 +", 4)
 
     # def test_buffer_add_mult(self):
     #     self.run_test("4 5 + 6 *",54)
@@ -43,6 +43,6 @@ class TestBufferInput(unittest.TestCase):
         result = calc.perform_buffer("1 +")
         self.assertEqual(result, 6)
 
-        # 9+6 
+        # 9+6
         result = calc.perform_buffer("+")
         self.assertEqual(result, 15)
